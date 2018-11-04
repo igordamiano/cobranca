fa4dec604c9c67f8885f9f5a595cce00ae8392df
irdbr <irdbr@IRDBR>
2018-11-04 17:47:51 -0200

    9

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -47,6 +47,15 @@ public class TituloController {
 		return mv;
 	}
 	
+	@RequestMapping
+	public String pesquisar() {
+		
+		
+		return "PesquisaTitulos";
+	}
+	
+	
+	
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
 		return Arrays.asList(StatusTitulo.values());
